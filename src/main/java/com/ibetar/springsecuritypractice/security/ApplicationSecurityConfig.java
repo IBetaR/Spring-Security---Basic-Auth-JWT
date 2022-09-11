5440b31aa0557ec2130681bb10e03f8f29c3dcec
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-09-11 19:59:01 -0300

    SSP C001: Student Entity, Users controller, end points created by role and permissions/authorities using Basic Auth

---
+++ src/main/java/com/ibetar/springsecuritypractice/security/ApplicationSecurityConfig.java
@@ -0,0 +1,90 @@
+package com.ibetar.springsecuritypractice.security;
+
+import com.ibetar.springsecuritypractice.persistence.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.ibetar.springsecuritypractice.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.ibetar.springsecuritypractice.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
+
+    //WebSecurityConfigurerAdapter
+//    @Override
+//    public ApplicationSecurityConfig(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers(HttpMethod.DELETE,"/manage/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/manage/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/manage/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/manage/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() {
+       UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+//                .roles(ApplicationUserRole.STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ApplicationUserRole.ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails aruUser = User.builder()
+                .username("aru")
+                .password(passwordEncoder.encode("password321"))
+//                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+       return new InMemoryUserDetailsManager(
+               annaSmithUser,
+               aruUser,
+               lindaUser
+       );
+    }
+}

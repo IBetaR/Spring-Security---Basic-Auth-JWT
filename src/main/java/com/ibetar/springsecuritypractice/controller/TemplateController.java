3d44556a3fc4f6d551a17fc0adc724789115c2b6
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-09-12 21:47:15 -0300

    SSP C002: Form base authentication using MVC - thymeleaf, login and logout buttons, sessionID uses and characteristics

---
+++ src/main/java/com/ibetar/springsecuritypractice/controller/TemplateController.java
@@ -0,0 +1,21 @@
+package com.ibetar.springsecuritypractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+
+    @GetMapping(value = "login")
+    public String getLoginView(){
+        return "login";
+    }
+
+    @GetMapping(value = "courses")
+    public String getCourses(){
+        return "courses";
+    }
+
+}
--- src/main/java/com/ibetar/springsecuritypractice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ibetar/springsecuritypractice/security/ApplicationSecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.ibetar.springsecuritypractice.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.ibetar.springsecuritypractice.security.ApplicationUserRole.*;
@@ -44,7 +49,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf().disable()
+                .csrf().disable()
+//                .and()
                 .authorizeRequests()
                 .antMatchers("/","index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
@@ -55,7 +61,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/courses",true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                    .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                        .key("Something very secured")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//delete this line if csrf is enabled
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
